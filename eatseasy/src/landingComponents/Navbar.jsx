2b1733fe80a295256cf464e97a63584a47fe726d
asad <ah2024@kent.ac.uk>
2025-02-03 23:52:15 +0000

    US008 - LandingPageV2
    Fixed bug Dan identified. Now when the mobile menu is opened, changing to desktop screen size will now hide the mobile menu and not stay stuck open.
    
    Also added some comments and spaced out the code to easily identify each part of the navbar component.

--- eatseasy/src/landingComponents/Navbar.jsx
+++ eatseasy/src/landingComponents/Navbar.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
+    const [nav, setNav] = useState(false);
 
     const handleNav = () => {
-        setNav(!nav)
-    }
+        setNav(!nav);
+    };
 
     return (
         <div className='flex justify-between items-center h-24 text-white bg-[#07853D] w-full px-6'>
-            {/* Adjusted logo size for better fit */}
+            {/* Logo */}
             <img className='h-12' src="/logo192.png" alt="EatsEasy Logo" />
 
+            {/* Desktop Menu -- home and log in and sign up buttons */}
             <ul className='hidden md:flex items-center space-x-6'>
                 <li className='text-lg font-bold'>Home</li>
                 <li>
@@ -22,10 +23,14 @@ const Navbar = () => {
                     <button className='bg-[#13290C] text-white px-4 py-2 rounded-md'>Sign up</button>
                 </li>
             </ul>
-            <div onClick={handleNav} className='block md:hidden'>
+
+            {/* Mobile Menu Button -- burger icon and exit out icon */}
+            <div onClick={handleNav} className='block md:hidden cursor-pointer'>
                 {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
             </div>
-            <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#4FC458] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+
+            {/* Mobile Menu -- slides out on the left */}
+            <div className={`${nav ? 'left-0' : 'left-[-100%]'} fixed top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#4FC458] ease-in-out duration-500 md:hidden`}>
                 <img className='h-12 m-4' src="/logo192.png" alt="EatsEasy Logo" />
                 <ul className='pt-12 space-y-2 p-4'>
                     <li className='text-lg font-bold p-4 border-b-2'>Home</li>
@@ -33,7 +38,6 @@ const Navbar = () => {
                     <li className='text-lg font-bold p-4'>Sign up</li>
                 </ul>
             </div>
-
         </div>
     );
 };
