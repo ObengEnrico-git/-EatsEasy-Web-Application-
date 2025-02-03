241df5e15daf851fd9eb8d55e3aeb7f2982cd3a4
asad <ah2024@kent.ac.uk>
2025-02-03 15:20:56 +0000

    LandingPageV2
    Landing Page version 2. Designed using components now. Added ReactTyped and The landing page is scale-able (should be compatible with any device size - mobile, desktop, iPad etc). V3 will add more components

---
+++ eatseasy/src/landingComponents/Navbar.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+    return (
+        <div className='flex justify-between items-center h-24 text-white bg-[#07853D] w-full px-6'>
+            {/* Adjusted logo size for better fit */}
+            <img className='h-12' src="/logo192.png" alt="EatsEasy Logo" />
+
+            <ul className='hidden md:flex items-center space-x-6'>
+                <li className='text-lg font-bold'>Home</li>
+                <li>
+                    <button className='bg-[#13290C] text-white px-4 py-2 rounded-md'>Log in</button>
+                </li>
+                <li>
+                    <button className='bg-[#13290C] text-white px-4 py-2 rounded-md'>Sign up</button>
+                </li>
+            </ul>
+            <div onClick={handleNav} className='block md:hidden'>
+                {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+            </div>
+            <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#4FC458] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+                <img className='h-12 m-4' src="/logo192.png" alt="EatsEasy Logo" />
+                <ul className='pt-12 space-y-2 p-4'>
+                    <li className='text-lg font-bold p-4 border-b-2'>Home</li>
+                    <li className='text-lg font-bold p-4 border-b-2'>Log in</li>
+                    <li className='text-lg font-bold p-4'>Sign up</li>
+                </ul>
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
